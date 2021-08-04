b2be5e66e030edc9c72280d0f67447fa5ae9169a
pontifex <lucas.wawrzyniak@gmail.com>
2021-08-04 23:03:38 +0200

    Run unit tests during CI

--- bin/travis_ci_before_script.sh
+++ bin/travis_ci_before_script.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
+cp .env.example .env
+
 cd laradock || exit
 cp ../.env.laradock .env
-docker-compose up -d nginx mysql redis elasticsearch
+docker-compose up -d nginx
 cd ..
 
-docker ps
-CONTAINER_NAME=$(docker ps -aqf "ancestor=automonde_workspace")
+CONTAINER_WORKSPACE_ID=$(docker ps -aqf "ancestor=automonde_workspace")
 
-docker exec "$CONTAINER_NAME" composer install;
+docker exec "$CONTAINER_WORKSPACE_ID" composer install;
+docker exec "$CONTAINER_WORKSPACE_ID" vendor/bin/phpunit;
+
 
 cd laradock || exit
 docker-compose down
